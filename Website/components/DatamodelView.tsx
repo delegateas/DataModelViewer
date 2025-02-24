721b64277aa4be5cc5d5cec30c776cea96c3dbec
Magnus Gether Sørensen <magesoe@gmail.com>
2025-02-24 13:59:24 +0100

    Made sidebar trigger placement dynamic

--- Website/components/DatamodelView.tsx
+++ Website/components/DatamodelView.tsx
@@ -6,10 +6,14 @@ import { AppSidebar } from "./AppSiderbar";
 import List from "./List";
 import { TooltipProvider } from "./ui/tooltip";
 import { TouchProvider } from "./ui/hybridtooltop";
-import { SidebarTrigger } from "./ui/sidebar";
+import { SidebarTrigger, useSidebar } from "./ui/sidebar";
+import { Button } from "./ui/button";
+import { PanelLeftOpen } from "lucide-react";
 
 export function DatamodelView() {
     const [selected, setSelected] = useState<string | null>(null);
+
+    const { open: isSidebarOpen, toggleSidebar } = useSidebar();
 
     const searchParams = useSearchParams()
     const entityParam = searchParams.get('selected')
@@ -19,9 +23,18 @@ export function DatamodelView() {
 
     return <>
         <AppSidebar selected={selected} onSelect={entity => setSelected(entity)} />
-        <SidebarTrigger className="top-0 sticky" />
+        {!isSidebarOpen && <Button
+            data-sidebar="trigger"
+            variant="ghost"
+            size="sm"
+            className="h-10 w-10 [&_svg]:size-6 top-0 sticky"
+            onClick={toggleSidebar}
+        >
+            <PanelLeftOpen />
+            <span className="sr-only">Toggle Sidebar</span>
+        </Button>}
         <div className='flex-1 flex flex-col min-w-0 overflow-auto'>
-            <div className='pr-5 pt-5 pb-20'>
+            <div className="pr-5 pt-5 pb-20">
                 <TouchProvider>
                     <TooltipProvider delayDuration={0}>
                         <List selected={selected} onSelect={entity => setSelected(entity)} />
