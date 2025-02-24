6b53ed42fa61aef8f70a6e05f3ffd8172be7db41
Magnus Gether Sørensen <magesoe@gmail.com>
2025-02-24 14:20:57 +0100

    Revoked sidebar changes

--- Website/components/AppSiderbar.tsx
+++ Website/components/AppSiderbar.tsx
@@ -5,12 +5,10 @@ import {
   SidebarContent,
   SidebarFooter,
   SidebarHeader,
-  useSidebar,
 } from "@/components/ui/sidebar"
 import { Button } from "./ui/button"
 import { deleteSession } from "@/lib/session";
 import NavItems from "./NavItems";
-import { PanelLeftClose } from "lucide-react";
 
 export function AppSidebar({
   selected,
@@ -18,24 +16,11 @@ export function AppSidebar({
 }: {
   selected: string | null
   onSelect: (entity: string) => void
-}) {
-
-  const { toggleSidebar } = useSidebar();
-  
+}) {  
   return (
     <Sidebar>
-      <SidebarHeader className="flex flex-row items-center justify-between">
+      <SidebarHeader>
         <Button onClick={() => deleteSession()}>Sign Out</Button>
-        <Button
-            data-sidebar="trigger"
-            variant="ghost"
-            size="sm"
-            className="h-10 w-10 [&_svg]:size-6 top-0 sticky"
-            onClick={toggleSidebar}
-        >
-            <PanelLeftClose />
-            <span className="sr-only">Toggle Sidebar</span>
-        </Button>
       </SidebarHeader>
       <SidebarContent>
         <NavItems selected={selected} onSelect={onSelect} />
--- Website/components/DatamodelView.tsx
+++ Website/components/DatamodelView.tsx
@@ -6,15 +6,12 @@ import { AppSidebar } from "./AppSiderbar";
 import List from "./List";
 import { TooltipProvider } from "./ui/tooltip";
 import { TouchProvider } from "./ui/hybridtooltop";
-import { useSidebar } from "./ui/sidebar";
 import { Button } from "./ui/button";
 import { PanelLeftOpen } from "lucide-react";
+import { SidebarTrigger } from "./ui/sidebar";
 
 export function DatamodelView() {
     const [selected, setSelected] = useState<string | null>(null);
-
-    const { open: isSidebarOpen, toggleSidebar } = useSidebar();
-
     const searchParams = useSearchParams()
     const entityParam = searchParams.get('selected')
     useEffect(() => {
@@ -23,16 +20,7 @@ export function DatamodelView() {
 
     return <>
         <AppSidebar selected={selected} onSelect={entity => setSelected(entity)} />
-        {!isSidebarOpen && <Button
-            data-sidebar="trigger"
-            variant="ghost"
-            size="sm"
-            className="h-10 w-10 [&_svg]:size-6 top-0 sticky"
-            onClick={toggleSidebar}
-        >
-            <PanelLeftOpen />
-            <span className="sr-only">Toggle Sidebar</span>
-        </Button>}
+        <SidebarTrigger />
         <div className='flex-1 flex flex-col min-w-0 overflow-auto'>
             <div className="pr-5 pt-5 pb-20">
                 <TouchProvider>
