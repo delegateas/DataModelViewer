028d1dfa1b344fc12950aa16c612652542444dd3
boer <boer@itu.dk>
2025-07-02 18:42:33 +0200

    chore: favicon

--- Website/components/AppSiderbar.tsx
+++ Website/components/AppSiderbar.tsx
@@ -11,8 +11,8 @@ export const AppSidebar = ({ children }: IAppSidebarProps) => {
   const isMobile = useIsMobile();
 
   return (
-    <div className="flex flex-col h-screen w-64 border-r border-gray-300 sticky top-0">
-      <div className="w-full h-16 border-b border-gray-300 p-2">
+    <div className="flex flex-col h-screen w-64 border-r border-sidebar-border sticky top-0 bg-sidebar">
+      <div className="w-full h-16 border-b border-sidebar-border p-2">
         { isMobile ? <img src="/DMVLOGO.svg" alt="Logo" className="h-full" draggable={false} /> : <img src="/DMVLOGOHORZ.svg" alt="Logo" className="h-full" draggable={false} />}
       </div>
       <div className="flex flex-grow">
