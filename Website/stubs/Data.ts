6ecf9d0a784b0a31d778c717c8cb8167387dfe5f
boer <boer@itu.dk>
2025-07-09 21:28:40 +0200

    chore: due to generated fil removed from git index, the issue came back... copy in workflow instead

---
+++ Website/stubs/Data.ts
@@ -0,0 +1,11 @@
+/// Used in github workflow to generate stubs for data
+/// This file is a stub and should not be modified directly.
+
+import { GroupType } from "@/lib/Types";
+
+export let Groups: GroupType[] = [
+  {
+    "Name":"Untitled",
+    "Entities":[]
+  }
+];
