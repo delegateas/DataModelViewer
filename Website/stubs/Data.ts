7fde87d7742838a446e505bab5d8e6f4ef53e3a5
boer <boer@itu.dk>
2025-07-26 14:20:37 +0200

    fix: added lastsync to stub

--- Website/stubs/Data.ts
+++ Website/stubs/Data.ts
@@ -2,7 +2,7 @@
 /// This file is a stub and should not be modified directly.
 
 import { GroupType } from "@/lib/Types";
-
+export const LastSynched: Date = new Date()
 export let Groups: GroupType[] = [
   {
     "Name":"Untitled",
