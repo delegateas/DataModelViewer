4c0e9bc2cdf9a53852a6469318d587a18513bb7c
Lucki2g <hardkaare123@gmail.com>
2025-08-27 21:41:16 +0200

    fix: fallback to stub

--- Website/app/api/markdown/route.ts
+++ Website/app/api/markdown/route.ts
@@ -3,7 +3,13 @@ import { readFileSync } from "fs";
 import { join } from "path";
 
 export async function GET() {
-    const filePath = join(process.cwd(), 'generated', 'Introduction.md');
-    const fileContent = readFileSync(filePath, 'utf-8');
+    const generatedPath = join(process.cwd(), 'generated', 'Introduction.md');
+    const stubsPath = join(process.cwd(), 'stubs', 'Introduction.md');
+    let fileContent;
+    try {
+        fileContent = readFileSync(generatedPath, 'utf-8');
+    } catch (err) {
+        fileContent = readFileSync(stubsPath, 'utf-8');
+    }
     return NextResponse.json({ fileContent })
 }
