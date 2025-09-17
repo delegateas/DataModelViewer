8c4beada03316c8c11f0bbdd921d57fd7f93cc20
Lucki2g <hardkaare123@gmail.com>
2025-09-17 19:26:54 +0200

    hotfix: found turbopack issue from next git

--- Website/next.config.ts
+++ Website/next.config.ts
@@ -1,6 +1,12 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  outputFileTracingRoot: __dirname,
+  turbopack: {
+    root: path.join(__dirname, '..'),
+  },
 }
 
 module.exports = nextConfig
