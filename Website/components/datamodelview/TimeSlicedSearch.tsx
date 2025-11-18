7772903f81ea214e2d545198031a6b8e05cc83d7
Lucki2g <hardkaare123@gmail.com>
2025-11-18 17:26:53 +0100

    chore: adjustments to search box on metadata

--- Website/components/datamodelview/TimeSlicedSearch.tsx
+++ Website/components/datamodelview/TimeSlicedSearch.tsx
@@ -237,7 +237,7 @@ export const TimeSlicedSearch = ({
   };
 
   const searchInput = (
-    <Box className={`fixed top-20 right-0 z-50 transition-opacity bg-transparent duration-200 ${shouldHideSearch ? 'opacity-0 pointer-events-none' : 'opacity-100'}`}>
+    <Box className={`fixed flex top-20 md:top-24 m-auto w-full justify-center md:justify-end md:right-4 z-50 transition-opacity bg-transparent duration-200 ${shouldHideSearch ? 'opacity-0 pointer-events-none' : 'opacity-100'}`}>
       <Paper component="form" className='p-1 rounded-lg flex items-center w-[320px]' sx={{ backgroundColor: 'background.paper' }}>
         <InputAdornment position="start" className='ml-1'>
           <SearchRounded color="action" />
